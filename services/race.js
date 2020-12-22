c643a47432aa67a8ca8be364e753f5a8da6385e7
BenJones <71987634+BenJonesMakers@users.noreply.github.com>
2020-12-22 12:30:09 +0000

    capturing a readable com port event - Yesssss

--- services/race.js
+++ services/race.js
@@ -1,6 +1,8 @@
 var RaceModel = require('../models/raceModel');
 let serialport = require('serialport');
 var plib = require('p3lib');
+const Readline = require('@serialport/parser-readline')
+const ByteLength = require('@serialport/parser-byte-length')
 //const { StartListening } = require('../controllers/races');
 
 var Race = {
@@ -47,18 +49,37 @@ var Race = {
         console.log('Im listening to port: ', portToUse);
         
         const port = new serialport(portToUse, {
-            baudRate: 57600
+            baudRate: 9600,
+            stopBits: 1,
+            parity: 'none',
+            dataBits: 8,
+            flowControl: false
           })
         
-        port.on('open', function(){
-            console.log('Serial Port Opened');
-            port.on('data', function(data){
-                console.log('data: ', data);
-                console.log('data at array 0: ', data[0]);
-                var decoded = ParserJs.decode(data[0]);
-                console.log('decoded', decoded);
-            });
-        });
+        recData = [];
+        const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+        //const parser = port.pipe(new ByteLength({length: 64}))
+        parser.on('data', function(data) {
+            
+            recData.push(data.toString());
+            console.log(recData);
+        })
+        
+        
+        
+        
+        
+        // port.on('open', function(){
+        //     console.log('Serial Port Opened');
+        //     port.on('data', function(data){
+        //         console.log('data: ', data);
+        //         console.log('data at array 0: ', data[0]);
+        //         //var decoded = ParserJs.decode(data);
+        //         //console.log('decoded', decoded);
+        //         var decodedTest = ParserJs.decode("8e023300e5630000010001047a00000003041fd855000408589514394cd8040005026d0006025000080200008104501304008f");
+        //         console.log('decodedTest', decodedTest);
+        //     });
+        // });
         
     }
 }
