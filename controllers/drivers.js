0b737c49684fc8fc64471c5b4793a66b344e0489
BenJones <71987634+BenJonesMakers@users.noreply.github.com>
2021-07-28 19:15:15 +0100

    Added ability to add new driver via API

--- controllers/drivers.js
+++ controllers/drivers.js
@@ -2,18 +2,14 @@ var Driver = require('../services/driver');
 
 var driverController = {
     New: async (req, res) => {
-        const newDriver = req.body;
-
-        let savedDriver = await Driver.Signup(newDriver);
-
+        const { transponderId, realName } = req.body;
+        let savedDriver = await Driver.Signup(transponderId, realName);
         res.json({ driver: savedDriver })
-
     },
 
     Index: async (req, res) => {
         let drivers = await Driver.GetAll();
         res.json(drivers);
-
     }
 }
 
--- services/driver.js
+++ services/driver.js
@@ -1,4 +1,5 @@
 const sqlite3 = require('sqlite3').verbose();
+const { resolve } = require('bluebird');
 const Promise = require('bluebird');
 let db = new sqlite3.Database('./db/races.db', (err) => {
     if (err) {
@@ -10,11 +11,28 @@ let db = new sqlite3.Database('./db/races.db', (err) => {
 
 var Driver = {
 
-    async Signup(user) {
+    async Signup(transponderId, realName) {
+        if (transponderId && realName) {
+            const messageParams = {
+                $transponderId: transponderId,
+                $realName: realName
+            }
 
-        // const driverRecord = await DriverModel.create(user);
-        // return driverRecord;
+            let sql = 'INSERT INTO drivers(transponderId, realName) VALUES($transponderId ,$realName)';
 
+            return new Promise((resolve, reject) => {
+                db.run(sql, messageParams, (err, res) => {
+                    if (err) {
+                        console.error(err.message);
+                        reject(err)
+                    } else {
+                        return resolve(res);
+                    }
+                })
+            })
+        } else {
+            console.log('No Data Recieved');
+        }
     },
 
     GetAll() {
